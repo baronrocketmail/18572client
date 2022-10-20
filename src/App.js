64dc7c0a85f1f92d602d9ccf0cf223b370b4df3f
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 08:30:34 -0700

    initial2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App(){
+    return(
+        <div className={"firstPage"}>
+            <h1>All Payments Up to Date</h1>
+            <div className={"linkBar"}>
+                <h4 className={"itemOne"}>cybernetic stream</h4>
+                <h4 className={"itemTwo"}>autopay</h4>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <div>
-      <div className={"firstPage"}>
-     <h1>All Payments Up to Date</h1>
-     <div className={"linkBar"}>
-         <h4 className={"itemOne"}>cybernetic stream</h4>
-         <h4 className={"itemTwo"}>autopay</h4>
-     </div>
-
-      </div>
-          <video  controls muted autoPlay={true} width={"100%"}>
-              <source src="https://storage.googleapis.com/ponn/1080P_4000K_410269351.mp4" type="video/mp4"/>
-                      Your browser does not support the video tag.
-          </video>
-          <img width="100%" src={"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-spaceblack?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1663703841897"}/>
-      </div>
+      <App/>
   </React.StrictMode>
 );
