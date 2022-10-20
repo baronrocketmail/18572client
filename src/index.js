2c749a30d8b36ff81c889b6ebe43bcc13857c5da
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 08:16:46 -0700

    initial

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ root.render(
      </div>
 
       </div>
+          <video  controls muted autoPlay={true} width={"100%"}>
+              <source src="https://storage.googleapis.com/ponn/1080P_4000K_410269351.mp4" type="video/mp4"/>
+                      Your browser does not support the video tag.
+          </video>
           <img width="100%" src={"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-spaceblack?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1663703841897"}/>
       </div>
   </React.StrictMode>
