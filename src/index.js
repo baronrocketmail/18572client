a6bfcff8bc346341e362231b46101ee34c09a652
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 08:02:26 -0700

    initial

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <div>
+      <div className={"firstPage"}>
      <h1>All Payments Up to Date</h1>
      <div className={"linkBar"}>
          <h4 className={"itemOne"}>cybernetic stream</h4>
          <h4 className={"itemTwo"}>autopay</h4>
      </div>
 
-
+      </div>
+          <img width="100%" src={"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-spaceblack?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1663703841897"}/>
       </div>
   </React.StrictMode>
 );
