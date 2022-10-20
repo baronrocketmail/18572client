4f45b56738365358c371651c972e32a356b25f47
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 07:50:51 -0700

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <div>
+     <h1>All Payments Up to Date</h1>
+     <div className={"linkBar"}>
+         <h4 className={"itemOne"}>cybernetic stream</h4>
+         <h4 className={"itemTwo"}>autopay</h4>
+     </div>
+
+
+      </div>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
